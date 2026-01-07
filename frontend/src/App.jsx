7498e52f875f5af06568e88a52773d754cef2e5b
g.nwuha <gnwuhajr@gmail.com>
2026-01-06 19:12:57 -0500

    TaskForm created for user input

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import TaskList from './components/TaskList'
+import TaskForm from './components/TaskForm'
 
 function App() {
   return (
     <div>
       <h1>Productivity AI</h1>
       <TaskList/>
+      <TaskForm/>
       <p>Welcome to your productivity app!</p>
     </div>
   )
---
+++ frontend/src/components/TaskForm.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import axios from 'axios'
+
+function TaskForm() {
+    const [title, setTitle] = useState('')
+    const [category, setCategory] = useState('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        axios.post('http://localhost:8000/tasks', null, {
+            params: {title, category}
+        })
+        .then(() => {
+            setTitle('')
+            setCategory('')
+            alert('Task created!')
+        })
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <h2>Add Task</h2>
+            <input
+            type="text"
+            placeholder='Task Title'
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            />
+
+            <input
+                type="text"
+                placeholder="Category"
+                value={category}
+                onChange={(e) => setCategory(e.target.value)}
+            />
+            <button type="submit">Add Task</button>
+        </form>
+    )
+}
+
+export default TaskForm
